8a9900a0f9cf1540b07015d262c8833b8fcf67f2
Nick <nicholasme@sunshinetechsolutions.com>
2019-10-20 14:33:21 -0700

    first commit

---
+++ src/main/java/sts/todo/TodoController.java
@@ -0,0 +1,16 @@
+package sts.todo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class TodoController {
+
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Todo!";
+    }
+
+}
---
+++ src/main/java/sts/todo/TodoService.java
@@ -0,0 +1,7 @@
+package sts.todo;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.regions.Regions;
+
+public class TodoService {
+}
